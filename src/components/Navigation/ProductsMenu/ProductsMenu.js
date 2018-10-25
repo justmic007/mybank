9686ee20abc12d05baec3d8d7d44a9cf68d421e2
Micah Joel <justmic007@gmail.com>
2018-10-25 20:35:02 +0100

    flexed product menu in one component

--- src/components/Navigation/ProductsMenu/ProductsMenu.js
+++ src/components/Navigation/ProductsMenu/ProductsMenu.js
@@ -25,25 +25,53 @@ const ProductsMenu = () => (
             <a href="/">Joint Mortgage</a>
             <a href="/">Get a Loan for Houseold Equipment</a>
             <a href="/">Automobile Loan</a>
-            <a href="/">Personal Loan against Salary</a>
+            <a href="/">Personal Loan Against Salary</a>
 
           </div>
         </div>
 
-        <div>
+        <div className={classes.dropdownMoneyTransfers} >
           <button>Money Transfers</button>
+          <div className={classes.dropdownMoneyTransfersContent}>
+            <a href="/">Moneygram</a>
+            <a href="/">Transfast</a>
+            <a href="/">Western Union Money Transfer</a>
+            <a href="/">Ria Money Transfer</a>
+          </div>
         </div>
 
-        <div>
+        <div className={classes.dropdownWaysToBank}>
           <button>Ways to Bank</button>
+            <div className={classes.dropdownWaysToBankContent}>
+              <a href="">Online Banking</a>
+              <a href="">React Mobile Apps</a>
+              <a href="">Cards</a>
+              <a href="">Reactmonie Agent</a>
+              <a href="">Payment Channels</a>
+              <a href="">Online Security</a>
+          </div>
         </div>
 
-        <div>
+        <div className={classes.dropdownCards}>
           <button>Cards</button>
+          <div className={classes.dropdownCardsContent}>
+            <a href="">Debit Cards</a>
+            <a href="">Credit Cards</a>
+            <a href="">Prepaid Cards</a>
+            <a href="">Perfect Summer Fit</a>
+          </div>
         </div>
 
-        <div>
+        <div className={classes.dropdownPaymentSolutions}>
           <button>Payment Solutions</button>
+          <div className={classes.dropdownPaymentSolutionsContent}>
+            <a href="">E-Payments</a>
+            <a href="">Bills and Utilities Payments</a>
+            <a href="">Schools Solutions</a>
+            <a href="">Collections</a>
+            <a href="">Trade</a>
+            <a href="">Taxes and Duties</a>
+          </div>
         </div>
 
   </div>
