f92184881596abc51edd1ab78cb2b1f34ee9bd7a
justmic007 <justmic007@gmail.com>
2018-11-27 15:44:23 +0100

    appBar now responsive

--- src/components/Navigation/AppBar/AppBar.js
+++ src/components/Navigation/AppBar/AppBar.js
@@ -1,20 +1,62 @@
 import React from 'react';
 
-import classes from './AppBar.css';
-
-import AboutUs from '../DropDown/AboutUs/AboutUs';
-import BankWithUs from '../DropDown/BankWithUs/BankWithUs';
-import ContactUs from '../DropDown/ContactUs/ContactUs';
-import Countries from '../DropDown/Countries/Countries';
+import classes from './AppBar.css'
 
 const AppBar = () => (
-  <div className={classes.Appbar}>
-    <AboutUs />
-    <BankWithUs />
-    <ContactUs />
-    <Countries />
 
+<div className={classes.navigationBar}>
+  <div className={classes.navitemAboutUs}>
+    <div>
+        <a href="/">About Us &nbsp;<i class="fa fa-caret-down fa-lg"></i></a>
+    </div>
+    <div className={classes.navitemAboutUsdropdownContent}>
+      <a href="/">Welcome</a>
+      <a href="/">Media</a>
+      <a href="/">Careers</a>
+    </div>
   </div>
 
+  <div className={classes.navitemBankWithUs}>
+    <div>
+        <a href="/">Bank With Us &nbsp;<i class="fa fa-caret-down fa-lg"></i></a>
+    </div>
+    <div className={classes.navitemBankWithUsdropdownContent}>
+          <a href="/">Personal</a>
+          <a href="/">Business</a>
+          <a href="/">Private Banking</a>
+    </div>
+  </div>
+
+  <div className={classes.navitemContactUs}>
+    <div>
+        <a href="/">Contact Us &nbsp;<i class="fa fa-caret-down fa-lg"></i></a>
+    </div>
+    <div className={classes.navitemContactUsdropdownContent}>
+          <a href="/">Whistle Blowing</a>
+          <a href="/">Whistle</a>
+    </div>
+  </div>
+
+
+  <div className={classes.navitemCountries}>
+    <div>
+        <a href="/">Nigeria &nbsp;<i class="fa fa-caret-down fa-lg"></i></a>
+    </div>
+    <div className={classes.navitemCountriesdropdownContent}>
+    <a href="/">China</a>
+      <a href="/">DRC</a>
+      <a href="/">Ghana</a>
+      <a href="/">Serria Leone</a>
+      <a href="/">Senegal</a>
+      <a href="/">Ghana</a>
+      <a href="/">Guinea</a>
+      <a href="/">UK</a>
+      <a href="/">France</a>
+    </div>
+  </div>
+</div>
+
 )
-export default AppBar;
+
+
+export default AppBar
